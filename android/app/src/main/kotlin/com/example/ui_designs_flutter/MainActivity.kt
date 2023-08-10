09789c4c02b5e8286464784709ad02e164f81c41
Safa <safaanwar2211@gmail.com>
2023-08-10 12:58:34 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ui_designs_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_designs_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/LoginScreens/login_screen_one.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class LoginScreenOne extends StatefulWidget {
+  const LoginScreenOne({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreenOne> createState() => _LoginScreenOneState();
+}
+
+class _LoginScreenOneState extends State<LoginScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
