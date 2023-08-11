7ef8aa39480b0925028bee706827d77b7c065f96
Safa <safaanwar2211@gmail.com>
2023-08-11 15:53:43 -0700

    13 commit

--- lib/PlantScenaryUI/plant_screen_one.dart
+++ lib/PlantScenaryUI/plant_screen_one.dart
@@ -5,6 +5,104 @@ class PlantUIScreenOne extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 450,
+                  child: Image.network(
+                    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 100),
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        width: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        width: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        width: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        width: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                          style: TextStyle(
+                            color: Colors.black38,
+                            letterSpacing: 0.8,
+                            height: 1.5,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back_ios_new_rounded),
+                  ),
+                ),
+                Positioned(
+                  top: 420,
+                  right: 100,
+                  left: 10,
+                  child: Text("New \nNatural",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/PlantScenaryUI/plant_screen_two.dart
+++ lib/PlantScenaryUI/plant_screen_two.dart
@@ -1,156 +1,149 @@
-
 import 'package:flutter/material.dart';
 import 'package:ui_designs_flutter/PlantScenaryUI/plant_screen_one.dart';
+
 class PlantUIScreenTwo extends StatelessWidget {
   const PlantUIScreenTwo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  margin:
-                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const CircleAvatar(
-                        backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black87,
+                      child: Text(
+                        'W',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.menu,
+                        size: 30,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: SizedBox(
+                    height: 350,
+                    child: Center(
+                      child: RotatedBox(
+                        quarterTurns: 3,
                         child: Text(
-                          "W",
-                          style: TextStyle(color: Colors.white),
+                          "Natural\nIngredients",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 28.0),
                         ),
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.menu,
-                          size: 30.0,
-                        ),
-                      )
-                    ],
+                    ),
+                  )),
+                  Expanded(
+                    child: SizedBox(
+                      height: 350,
+                      child: Image.network(
+                        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    const Expanded(
-                      child: SizedBox(
-                        height: 350,
-                        child: Center(
-                          child: RotatedBox(
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 300,
+                      color: Colors.black87,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RotatedBox(
                             quarterTurns: 3,
-                            child: Text(
-                              "Natural\nIngredients",
+                            child: Text("01",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 28.0,
+                                    color: Colors.white)),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20),
+                            width: 1,
+                            height: 50,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                    height: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          child: Text("Info",
                               style: TextStyle(
-                                  fontWeight: FontWeight.w500, fontSize: 28.0),
+                                color: Colors.black,
+                                fontSize: 20.0,
+                              )),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30.0, vertical: 30.0),
+                          child: Text(
+                              "More and More people are option to the herbal lyfe",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                              )),
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>PlantUIScreenOne()));
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 40,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.black87,
+                                borderRadius: BorderRadius.circular(16),
+
+                              ),
+                              child: Text(
+                                "Read Me",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                    Expanded(
-                      flex: 2,
-                      child: SizedBox(
-                        height: 350,
-                        child: Image.network(
-                          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1032600956.jpg",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 300,
-                        color: Colors.black,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const RotatedBox(
-                              quarterTurns: 3,
-                              child: Text("01",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 28.0,
-                                      color: Colors.white)),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 20.0),
-                              width: 1,
-                              height: 50.0,
-                              color: Colors.white,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: SizedBox(
-                        height: 300,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 30),
-                              child: Text("Info",
-                                  style: TextStyle(
-                                    color: Colors.black45,
-                                    fontSize: 20.0,
-                                  )),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 30.0, vertical: 30.0),
-                              child: Text(
-                                  "More and More people are option to the herbal lyfe",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16.0,
-                                  )),
-                            ),
-                            Center(
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (BuildContext context) =>
-                                          const PlantUIScreenOne()));
-                                },
-                                child: Container(
-                                  alignment: Alignment.center,
-                                  height: 40,
-                                  width: 100,
-                                  decoration: BoxDecoration(
-                                    color: Colors.black,
-                                    borderRadius: BorderRadius.circular(16.0),
-                                  ),
-                                  child: const Text(
-                                    "Read More",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
+                  ))
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
