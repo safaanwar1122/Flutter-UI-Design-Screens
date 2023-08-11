c29cbc8e4d023193d76dd66e8e3ab405981ae124
Safa <safaanwar2211@gmail.com>
2023-08-11 00:04:51 -0700

    7th commit

--- lib/FoodAppUI/Pages/homepage.dart
+++ lib/FoodAppUI/Pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MenuePage extends StatefulWidget {
   const MenuePage({Key? key}) : super(key: key);
@@ -8,10 +9,218 @@ class MenuePage extends StatefulWidget {
 }
 
 class _MenuePageState extends State<MenuePage> {
+  int selectedCategory = 0;
+  List<String> categorie = ["Pizza", "Burgers", "Kebab", "Desert", "Salad"];
+  List<dynamic> foods = [
+    {
+      "image": "images/food.jpg",
+      "isFavourite": false,
+    },
+    {
+      "image": "images/hunger.jpg",
+      "isFavourite": false,
+    },
+    {
+      "image": "images/starter.jpg",
+      "isFavourite": false,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    List<String> categorie = ["Pizza", "Burgers", "Kebab", "Desert", "Salad"];
-
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[100],
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        leading: Icon(null),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_basket,
+                color: Colors.grey[800],
+              ))
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Food Delivery',
+                    style: TextStyle(
+                        color: Colors.grey[80],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categorie.length,
+                        itemBuilder: (context, index) =>
+                            makeCategory(
+                                title: categorie[index], index: index)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Free Delivery',
+                style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: foods.length,
+                      itemBuilder: (context, index) {
+                        makeItem(image: foods[index]["image"],
+                            isFavourite: foods[index]["isFavorite"],
+                            index: index);
+                      }),
+                ))
+          ],
+        ),
+      ),
+    );
   }
+
+  Widget makeCategory({title, index}) {
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedCategory = index;
+          });
+        },
+        child: Container(
+          width: 120,
+          margin: EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            color: selectedCategory == index
+                ? Colors.yellow[700]
+                : Colors.grey[300],
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: selectedCategory == index
+                      ? Colors.black87
+                      : Colors.grey[700],
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ));
+  }
+
+  Widget makeItem({required image, required isFavourite, required int index}) {
+    return AspectRatio(
+      aspectRatio: 1 / 1.5,
+      child: GestureDetector(
+        child: Container(
+          margin: EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              )),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  stops: [.2, .9],
+                  colors: [
+                    Colors.black87.withOpacity(.9),
+                    Colors.black87.withOpacity(.9),
+                  ],
+                )),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        foods[index]["isFavourite"] =
+                        !foods[index]["isFavourite"];
+                      });
+                    },
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                                width: 1.5,
+                                color: isFavourite
+                                    ? Colors.red
+                                    : Colors.transparent),
+                          ),
+                          child: isFavourite
+                              ? Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                              : Icon(
+                            Icons.favorite,
+                            color: Colors.white,
+                          )),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "\$ 15.00",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Vegetarian Pizza",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
